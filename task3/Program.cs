d0eed4b65d97669113faaf8b9ac3a4582642a7a4
Aleksandr <mealvasm@example.com>
2022-06-25 17:22:27 +0500

    Добавление task3

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 
+ * Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+ * 5 -> 2, 4
+ * 8 -> 2, 4, 6, 8
+ */
+
+ Console.WriteLine($"Введите N");
+ int num = Convert.ToInt32(Console.ReadLine());
+ int count = 1;
+
+ if( num > 0)
+ {
+    while(count <= num)
+ {
+    if(count % 2 == 0){ Console.WriteLine($"Найдено четное число в диапозоне от 1 до {num}: {count}");}
+    count++;
+ }
+ }
+ else
+ {
+    Console.WriteLine($"Введите число больше 0");
+ }
+
+
